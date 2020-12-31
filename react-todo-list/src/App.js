0bc7673cbeb8a402a3a37a19e79293381704c467
MaciejWitkowskiDev <d3lusion@post.cz>
2020-12-31 16:15:51 +0100

    Implemented the TaskList na Task components

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -1,12 +1,25 @@
 import Form from './Form';
 import React, {useState } from "react";
+import TaskList from './TaskList';
 
 function App() {
   const [taskList, setTaskList] = useState([]);
   
+  const addMockTodo = () => {
+    setTaskList([
+      ...taskList,
+      {
+        id: 0,
+        name: 'xD'
+      }
+    ]);
+  }
+
   return (
     <>
+      <TaskList listOfTasks={taskList} />
       <Form inputPlaceholder="Task name" buttonLabel="Add new todo" />
+      <button onClick={addMockTodo}>addMockTodo</button>
     </>
   );
 }
---
+++ react-todo-list/src/TaskList.js
@@ -0,0 +1,17 @@
+import React, {useState } from "react";
+import Task from './Task'
+
+function TaskList(props){
+    const tasks = props.listOfTasks.map(task => {
+        return(
+            <Task key={task.id} name = {task.name} /> 
+        );
+    });
+    return(
+       <ul>
+        {tasks}
+       </ul>
+    );
+}
+
+export default TaskList;
